11ee364fcf4f32fa9845ad334e08a3d5788952d8
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-05-06 10:45:27 +0200

    add ValidationException, SessionMiddleware, ValidationMiddleware

--- src/App/functions.php
+++ src/App/functions.php
@@ -1,9 +1,25 @@
 <?php
 
-function dump($value)
+declare(strict_types=1);
+
+use Framework\Http;
+
+function dump(mixed $value)
 {
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     die();
+}
+
+function escapeData(mixed $value): string
+{
+    return htmlspecialchars((string) $value);
+}
+
+function redirectTo(string $path)
+{
+    header("Location: {$path}");
+    http_response_code(Http::REDIRECT_STATUS_CODE);
+    exit;
 }
