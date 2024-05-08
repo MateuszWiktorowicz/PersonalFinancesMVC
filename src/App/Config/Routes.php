0db0372921a6b5ceaef090a1a037bd656752c4e2
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-05-08 08:04:06 +0200

    add logout feature

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -6,12 +6,14 @@ namespace App\Config;
 
 use App\Controllers\{HomeController, AuthController};
 use Framework\App;
+use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 
 function registerRoutes(App $app)
 {
     $app->getMethod('/', [HomeController::class, 'home']);
-    $app->getMethod('/register', [AuthController::class, 'registerView']);
-    $app->getMethod('/login', [AuthController::class, 'loginView']);
-    $app->post('/register', [AuthController::class, 'register']);
-    $app->post('/login', [AuthController::class, 'login']);
+    $app->getMethod('/register', [AuthController::class, 'registerView'])->add(GuestOnlyMiddleware::class);
+    $app->getMethod('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
+    $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
+    $app->getMethod('/logout', [AuthController::class, 'logout'])->add(AuthRequiredMiddleware::class);
 }
