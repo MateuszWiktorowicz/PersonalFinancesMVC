ff44923aa37c2c1f527da4e59d28d0b02af9b972
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-05-23 09:48:19 +0200

    add setting view

--- src/App/views/settings.php
+++
@@ -1,5 +0,0 @@
-<?php include $this->resolve("partials/_header.php"); ?>
-
-<body>
-    <h1>In progress...</h1>
-    <?php include $this->resolve("partials/_footer.php"); ?>
---
+++ src/App/views/settings/settings.php
@@ -0,0 +1,29 @@
+<?php include $this->resolve("partials/_header.php"); ?>
+
+<body>
+    <main>
+        <section class="sectionBox">
+            <div class="d-flex justify-content-center">
+                <h1>Settings</h1>
+            </div>
+        </section>
+        <section class="sectionBox">
+            <div class="d-flex flex-column align-items-center gap-4">
+                <div class="border-bottom border-black w-100 text-center">
+                    <h2>Edit profile</h2>
+                </div>
+                <div class="border-bottom border-black w-100 text-center">
+                    <h2>Edit income categories</h2>
+                </div>
+                <div class="border-bottom border-black w-100 text-center">
+                    <h2>Edit expense categories</h2>
+                </div>
+                <div class="border-bottom border-black w-100 text-center">
+                    <h2>Edit payment methods</h2>
+                </div>
+
+            </div>
+
+        </section>
+    </main>
+    <?php include $this->resolve("partials/_footer.php"); ?>
